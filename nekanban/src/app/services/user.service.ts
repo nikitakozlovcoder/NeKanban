d6027e4384af0892200f719c71b78e2deab9eb46
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2022-02-12 18:33:27 +0300

    2 commit

--- nekanban/src/app/services/user.service.ts
+++ nekanban/src/app/services/user.service.ts
@@ -1,3 +1,6 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+
 export class User {
     name: string;
     surname: string;
@@ -10,12 +13,39 @@ export class User {
       this.password = password;
     }
   }
+@Injectable()
 export class UserService {
+  name = "";
+  id = 0;
+  base_url = "https://localhost:7146/";
+  constructor(private http: HttpClient) { }
     private users : User[] = [new User("Alex", "Alexov", "a@gmail.com", "123456789")]
     getUsers() : User[] {
         return this.users;
     }
     addUser(name: string, surname: string, email: string, password: string) {
+      const body = {"email": email, "password": password, "name": name, "surname": surname};
+      const test_body = {title: "foo", body: "bar", userId: 1};
+      console.log(body);
+      /*this.http.post<any>("https://jsonplaceholder.typicode.com/posts", test_body).subscribe({
+        next: data => {
+          this.name = data.title;
+          this.id = data.id;
+          console.log(this);
+        },
+        error: error => {
+          console.error('There was an error!', error.message);
+        }
+      })*/
+      this.http.post<any>(this.base_url + "Users/Register", body).subscribe({
+        next: data => {
+          this.name = data.name;
+          console.log(this);
+        },
+        error: error => {
+          console.error('There was an error!', error.message);
+        }
+      })
         this.users.push(new User(name, surname, email, password));
     }
 }
