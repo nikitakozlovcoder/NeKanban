ebeb85c99f389786a308af845050fb2cb48c4813
Nikita Kozlov <nikita@thetourguy.com>
2023-02-23 14:17:47 +0300

    NK-21

---
+++ NeKanbanApi/Batteries/FileStorage/Entities/FileStorageEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Batteries.FileStorage.Entities;
+
+public class FileStorageEntity : IFileEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
---
+++ NeKanbanApi/Batteries/FileStorage/FileStorageAdapters/BaseFileStorageAdapter`2.cs
@@ -0,0 +1,15 @@
+﻿using Batteries.FileStorage.Entities;
+using Batteries.FileStorage.FileStorageProviders;
+using Batteries.Repository;
+
+namespace Batteries.FileStorage.FileStorageAdapters;
+
+public abstract class BaseFileStorageAdapter<TFileAdapterEntity, TParent> : BaseFileStorageAdapter<TFileAdapterEntity, TParent, FileStorageEntity>,
+    IFileStorageAdapter<TFileAdapterEntity, TParent>
+    where TFileAdapterEntity : class, IFileAdapterEntity<TParent>, new()
+    where TParent : IHasPk<int>
+{
+    protected BaseFileStorageAdapter(IFileStorageProvider provider, IRepository<TFileAdapterEntity> storeAdapterRepository, IRepository<FileStorageEntity> fileRepository) : base(provider, storeAdapterRepository, fileRepository)
+    {
+    }
+}
