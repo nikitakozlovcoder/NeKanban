c3da80f1a700321b7fb42d48edcecb8f327012ab
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2023-03-03 13:04:37 +0300

    NK-22 Added user personal data agreement

--- nekanban/src/app/components/auth/register/register.component.ts
+++ nekanban/src/app/components/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {UntypedFormControl, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {FormGroup, UntypedFormControl, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import { UserService } from '../../../services/user.service';
 import {BehaviorSubject} from "rxjs";
 
@@ -17,11 +17,15 @@ export class RegisterComponent implements OnInit {
 
   hide = true;
   hideConfirm = true;
+
   email = new UntypedFormControl('', [Validators.required, Validators.email]);
   name = new UntypedFormControl('', [Validators.required]);
   surname = new UntypedFormControl('', [Validators.required]);
   password = new UntypedFormControl('', [Validators.required, Validators.minLength(8)]);
   password_confirmation = new UntypedFormControl('', [Validators.required, this.passwordMatchValidator(this.password)]);
+  userAgreement = new UntypedFormControl(false, [Validators.requiredTrue]);
+
+
   getEmailErrorMessage() {
     if (this.email.hasError('required')) {
       return 'Поле не должно быть пустым!';
@@ -39,15 +43,17 @@ export class RegisterComponent implements OnInit {
   };
 
   registerUser() {
-    if (this.email.invalid || this.name.invalid || this.surname.invalid || this.password.invalid || this.password_confirmation.invalid) {
+    if (this.email.invalid || this.name.invalid || this.surname.invalid || this.password.invalid ||
+      this.password_confirmation.invalid || this.userAgreement.invalid) {
       this.name.markAsTouched();
       this.surname.markAsTouched();
       this.email.markAsTouched();
       this.password.markAsTouched();
       this.password_confirmation.markAsTouched();
+      this.userAgreement.markAsTouched();
     } else {
       this.busy.next(true);
-      this.userService.addUser(this.name.value, this.surname.value, this.email.value, this.password.value).subscribe({
+      this.userService.addUser(this.name.value, this.surname.value, this.email.value, this.password.value, this.userAgreement.value).subscribe({
         next: () => this.busy.next(false),
         error: () => this.busy.next(false)
       });
