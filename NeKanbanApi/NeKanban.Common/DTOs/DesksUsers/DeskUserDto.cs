98c9ea5a194cfe1d6bdf956fc959dc3b7303f5a6
Nikita Kozlov <nikita@thetourguy.com>
2023-03-25 14:25:39 +0300

    NK-29

--- NeKanbanApi/NeKanban.Common/DTOs/DesksUsers/DeskUserDto.cs
+++ NeKanbanApi/NeKanban.Common/DTOs/DesksUsers/DeskUserDto.cs
@@ -1,16 +1,15 @@
 ﻿using AutoMapper;
 using Batteries.Mapper.Interfaces;
 using NeKanban.Common.Constants;
-using NeKanban.Common.DTOs.Roles;
+using NeKanban.Common.DTOs.ApplicationUsers;
 using NeKanban.Common.Entities;
-using NeKanban.Common.ViewModels;
 using NeKanban.Common.ViewModels.Roles;
 
 namespace NeKanban.Common.DTOs.DesksUsers;
 
 public class DeskUserDto: BaseEntityModel<int>, IAutoMapFrom<DeskUser, DeskUserDto>
 {
-    public required ApplicationUserWithTokenVm? User { get; set; }
+    public required ApplicationUserDto? User { get; set; }
     public required RoleDto? Role { get; set; }
     public required bool IsOwner { get; set; }
     public required PreferenceType Preference { get; set; }
