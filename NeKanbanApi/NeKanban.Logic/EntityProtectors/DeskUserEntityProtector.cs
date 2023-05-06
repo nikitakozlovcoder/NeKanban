9a350543f09dfeb79e47975b3fc4905d7ca7368f
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2023-05-06 16:44:09 +0300

    NK-50 Fixes

--- NeKanbanApi/NeKanban.Logic/EntityProtectors/DeskUserEntityProtector.cs
+++ NeKanbanApi/NeKanban.Logic/EntityProtectors/DeskUserEntityProtector.cs
@@ -2,6 +2,7 @@
 using Batteries.Repository;
 using JetBrains.Annotations;
 using NeKanban.Common.Entities;
+using NeKanban.Data.Infrastructure.QueryFilters;
 using NeKanban.Logic.Services.Security;
 
 namespace NeKanban.Logic.EntityProtectors;
@@ -11,14 +12,20 @@ namespace NeKanban.Logic.EntityProtectors;
 public class DeskUserEntityProtector : BaseEntityProtector<DeskUser>
 {
     private readonly IRepository<DeskUser> _deskUserRepository;
+    private readonly QueryFilterSettings _filterSettings;
     public DeskUserEntityProtector(IRepository<DeskUser> deskUserRepository,
-        IPermissionCheckerService permissionCheckerService) : base(permissionCheckerService)
+        IPermissionCheckerService permissionCheckerService, QueryFilterSettings filterSettings) : base(permissionCheckerService)
     {
         _deskUserRepository = deskUserRepository;
+        _filterSettings = filterSettings;
     }
 
     protected override async Task<int?> GetDeskId(int entityId, CancellationToken ct)
     {
+        using var scope = _filterSettings.CreateScope(new QueryFilterSettingsDefinitions
+        {
+            DeskUserDeletedFilter = false
+        });
         var deskUser = await _deskUserRepository.First(x => x.Id == entityId, ct);
         return deskUser.DeskId;
     }
