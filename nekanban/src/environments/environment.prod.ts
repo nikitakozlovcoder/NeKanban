831a5da90d51f24680597dfe738215a01237e3cf
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2023-05-21 14:15:37 +0300

    NK-50 Added production configuration

--- nekanban/src/environments/environment.prod.ts
+++ nekanban/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: "https://nekanbanapi.herokuapp.com/"
+  baseUrl: "http://46.19.64.33:7146/",
+  angularBaseUrl: "http://localhost:4200/",
+  urlsToDisableAuthInterceptor: ['/Users/LogIn', '/Users/Refresh'],
+  placeholderImage: "data:image/webp;base64,UklGRuwBAABXRUJQVlA4IOABAADwHQCdASosAcsAPm02m0ikI6KhJNioWIANiWlu4XKRG5+ut2qOaZVBfsUFFL3FnfKKXuLO+UUvcWd8ope4s75RS9xZ3yidMia+gYQGN/iHIt2WPjRbfYoeSr9v87J5pYu6+Vid7vat3+wF7BZ3yil4ZkADyb4Fmm3rrOtg1dfCtZRWtKR63v1+MW+1Y+NExlG0iEZd2ni9HN6TxX5Lsa7WPjRbZpqvmS07EZGK/ZEY0W32rG2+nMuHWgCdqVgYQF57tZ0Fhe2VfAe1Y+NFprWQf37u32+18roIOyuQ4s75RS9xZ3yil7izvlFL3FnfKKXuLO+UUvcNAAD+/xGDsRzgQAA0lwlVCPFjsNFrCp8T8ilYyYfvH3OAIbhCU7m6ejBMJKuFnb9ev0uPVYxxahdPDBgawTIYMCc1p8z9xBJ3Q0+QX+HypvgNMY6c8JuqHOGSCGQ9cvUJgWwbIL6J6W+l1Ph1HgGN44WQlC2Ee5Bx/rH2W9S/TEUoCZc/YgePyoAJvaolbEF4JLjr0LM1qD3g+opWuWHOJMhknAAo25oPDFgttrvJZAAIYjnXUh4c4HkxtKV6/ZC5DSgYpqmYEJxwP3k2jNsPlP+uDSxaP/wTIjpfUjw2U+KDrKNAAAAAAAA="
 };
