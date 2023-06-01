2762e240b7c417123a8046a08f3313eea71e7e9e
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2023-06-01 18:07:40 +0300

    NK-50 Updated new assignment system to work with permissions & bug fixes

--- nekanban/src/app/components/todo/todo-show/todo-show.component.ts
+++ nekanban/src/app/components/todo/todo-show/todo-show.component.ts
@@ -1,10 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Todo} from "../../../models/todo";
-import {UntypedFormControl,} from "@angular/forms";
 import {Desk} from "../../../models/desk";
-import {User} from "../../../models/user";
-import {MatSelect} from "@angular/material/select";
 import {TodoService} from "../../../services/todo.service";
 import {RolesService} from "../../../services/roles.service";
 import {DeskUser} from "../../../models/deskUser";
@@ -20,12 +17,18 @@ import {ToDoUserType} from "../../../constants/ToDoUserType";
 export class TodoShowComponent implements OnInit {
 
   todo?: Todo;
-  usersSelected : number[] = [];
-  userSelected : number[] = [];
-  users = new UntypedFormControl(this.usersSelected);
-  user = new UntypedFormControl(this.userSelected);
-  areUsersLoaded = new BehaviorSubject(true);
   todoLoaded = new BehaviorSubject(false);
+  newAssigneeLoaded = new BehaviorSubject(true);
+  assigneesLoaded : BehaviorSubject<boolean>[] = [];
+  get freeUsers() {
+    if (this.rolesService.userHasPermission(this.data.roles, this.data.deskUser, this.rolesService.permissionsTypes.ManageAssigners)) {
+      return this.data.desk.deskUsers.filter(x => !this.todo!.toDoUsers.some(el => el.deskUser.id === x.id && el.toDoUserType !== ToDoUserType.Creator));
+    }
+    else if (this.rolesService.userHasPermission(this.data.roles, this.data.deskUser, this.rolesService.permissionsTypes.AssignTasksThemself)) {
+      return this.todo!.toDoUsers.some(x => x.deskUser.id === this.data.deskUser.id) ? [] : [this.data.deskUser];
+    }
+    return [];
+  }
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: {todoId: number, isEdit: boolean, desk: Desk, deskUser: DeskUser, roles: Role[]},
               private readonly toDoService: TodoService,
@@ -39,10 +42,7 @@ export class TodoShowComponent implements OnInit {
       next: value => {
         this.todo = value;
         this.todoLoaded.next(true);
-        this.usersSelected = this.getIdsOfSelectedUsers();
-        this.users.patchValue(this.usersSelected);
-        this.userSelected = this.getIdOfSingleUser();
-        this.user.patchValue(this.userSelected);
+        this.todo.toDoUsers.forEach(() => this.assigneesLoaded.push(new BehaviorSubject<boolean>(true)));
       }
     })
   }
@@ -54,158 +54,38 @@ export class TodoShowComponent implements OnInit {
   getToDoCreator() {
     return this.todo!.toDoUsers.find(el => el.toDoUserType == 0);
   }
-
-  getIdOfSingleUser() {
-    let ids : number[] = [];
-    let founded = this.getToDoUsers().find(el => el.deskUser.user.id === this.data.deskUser.user.id);
-    if (founded !== undefined) {
-      ids.push(founded.deskUser.user.id);
-    }
-    return ids;
-  }
-
   getToDoUsers() {
     return this.todo!.toDoUsers.filter(el => el.toDoUserType != 0);
   }
 
-  getDeskUsers() : User[] {
-    let deskUsers: User[] = [];
-    this.data.desk.deskUsers.forEach( el => {
-      deskUsers.push(el.user);
-    })
-    return deskUsers;
-  }
-
-  getFreeUsers() {
-    return this.data.desk.deskUsers.filter(x => !this.todo!.toDoUsers.some(el => el.deskUser.id === x.id && el.toDoUserType !== ToDoUserType.Creator));
-  }
-
-  getAllTodoUsers() : User[] {
-    let todoUsers : User[] = [];
-    this.todo!.toDoUsers.forEach( el => {
-      todoUsers.push(el.deskUser.user);
-    })
-    return todoUsers;
-  }
-
-  getIdsOfSelectedUsers() : number[] {
-    let selectedUsers : User[] = this.getDeskUsers().filter(el => this.getAllTodoUsers().some(someEl => someEl.id === el.id)
-      && this.todo!.toDoUsers.find(todoUser => todoUser.deskUser.user.id === el.id  && todoUser.toDoUserType != 0));
-    let ids : number[] = [];
-    selectedUsers.forEach( el => {
-      ids.push(el.id);
-    })
-    return ids;
-  }
-
-  changeUsers(select:MatSelect)  {
-    let newIds : number[] = select.value;
-    let appearedIds: number[] = [];
-    newIds.forEach(el => {
-      if (!this.usersSelected.includes(el)) {
-        appearedIds.push(el);
-      }
-    })
-    let disappearedIds : number[] = [];
-    this.usersSelected.forEach( el => {
-      if (!newIds.includes(el)) {
-        disappearedIds.push(el);
-      }
-    })
-
-    appearedIds.forEach(el => {
-      this.areUsersLoaded.next(false);
-      this.dialogRef.disableClose = true;
-      let deskUser = this.data.desk.deskUsers.find(obj => obj.user.id === el);
-      this.toDoService.assignUser(this.data.todoId, deskUser!.id).subscribe({
-        next: data => {
-          if (appearedIds.indexOf(el) === appearedIds.length - 1) {
-            this.areUsersLoaded.next(true);
-            this.dialogRef.disableClose = false;
-          }
-          this.todo!.toDoUsers = data.toDoUsers;
-        },
-        error: _ => {
-        }
-      })
-    })
-    disappearedIds.forEach( el => {
-      this.areUsersLoaded.next(false);
-      this.dialogRef.disableClose = true;
-      let todo = this.todo!.toDoUsers.find(obj => obj.deskUser.user.id === el && obj.toDoUserType != 0);
-      this.toDoService.removeUser(todo!.id).subscribe({
-        next: data => {
-          if (disappearedIds.indexOf(el) === disappearedIds.length - 1) {
-            this.areUsersLoaded.next(true);
-            this.dialogRef.disableClose = false;
-          }
-          this.todo!.toDoUsers = data.toDoUsers;
-        },
-        error: _ => {
-        }
-      })
-    })
-    this.usersSelected = newIds;
-    this.users.patchValue(this.usersSelected);
-  }
-
-  changeSingleUser(select:MatSelect) {
-    let newIds : number[] = select.value;
-    if (newIds.length === 0) {
-      let todo = this.todo!.toDoUsers.find(obj => obj.deskUser.user.id === this.data.deskUser.user.id);
-      if (todo !== undefined) {
-        this.areUsersLoaded.next(false);
-        this.dialogRef.disableClose = true;
-        this.toDoService.removeUser(todo.id).subscribe({
-          next: data => {
-            this.areUsersLoaded.next(true);
-            this.todo!.toDoUsers = data.toDoUsers;
-            this.dialogRef.disableClose = false;
-          },
-          error: _ => {
-          }
-        })
-      }
-
-    }
-    else {
-      if (!this.usersSelected.includes(this.data.deskUser.user.id)) {
-        this.areUsersLoaded.next(false);
-        this.dialogRef.disableClose = true;
-        this.toDoService.assignUser(this.data.todoId, this.data.deskUser.id).subscribe({
-          next: data => {
-            this.areUsersLoaded.next(true);
-            this.todo!.toDoUsers = data.toDoUsers;
-            this.dialogRef.disableClose = false;
-          },
-          error: _ => {
-          }
-        })
-      }
-    }
-  }
-
   assignUser(deskUserId: number) {
+    this.newAssigneeLoaded.next(false);
     this.toDoService.assignUser(this.data.todoId, deskUserId).subscribe({
       next: data => {
-        this.areUsersLoaded.next(true);
+        this.newAssigneeLoaded.next(true);
         this.todo!.toDoUsers = data.toDoUsers;
         this.dialogRef.disableClose = false;
       },
       error: _ => {
       }
+    }).add(() => {
+      this.assigneesLoaded = [];
+      this.todo!.toDoUsers.forEach(() => this.assigneesLoaded.push(new BehaviorSubject<boolean>(true)));
     })
   }
 
   removeUser(todoUserId: number) {
+    this.assigneesLoaded[this.todo!.toDoUsers.findIndex(x => x.id === todoUserId) - 1].next(false);
     this.toDoService.removeUser(todoUserId).subscribe({
       next: data => {
-        this.areUsersLoaded.next(true);
         this.todo!.toDoUsers = data.toDoUsers;
         this.dialogRef.disableClose = false;
       },
       error: _ => {
       }
+    }).add(() => {
+      this.assigneesLoaded = [];
+      this.todo!.toDoUsers.forEach(() => this.assigneesLoaded.push(new BehaviorSubject<boolean>(true)));
     })
   }
 }
